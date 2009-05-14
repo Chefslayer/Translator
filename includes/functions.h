f15e0d6d14ad7b9d16531a45578675e47ed69375
Robert Heim <robhei@kojak.informatik.RWTH-Aachen.DE>
2009-05-14 19:16:59 +0200

    doxygen

--- includes/functions.h
+++ includes/functions.h
@@ -9,7 +9,6 @@
 #include "../classes/SingleCount.h"
 
 using namespace std;
-
 vector<string> stringSplit(string str, string delim);
 vector<unsigned int> insertAndConvert2intVector(string line, Lexicon& lex, SingleCount& sinlgesObject);
 void showFreq(PairCount& pairs, SingleCount& singlesF, SingleCount& singlesE, Lexicon& f, Lexicon& e);
