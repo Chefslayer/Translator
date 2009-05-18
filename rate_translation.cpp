4bfaa25862e6eec689a94640914fed87672876fd
Chefslayer <inga.herber@web.de>
2009-05-18 11:29:49 +0200

    alles rausgelöscht was in bleu.cpp gehört

--- rate_translation.cpp
+++ rate_translation.cpp
@@ -28,68 +28,6 @@ unsigned int levenshtein_distance(string &trans, string &target)
 	return 0;
 }
 
-/**
-*
-*
-* \param n
-* \param H reference to the Hypothesis to count the n-grams for
-* \return 
-*/
-unsigned int count_n_grams(unsigned int n, Hypothesis* H)
-{
-	// TODO
-	return 0;
-}
-
-/**
-* calcs the precision of a Hypothesis with n-grams
-*
-* \param C reference to the Hypothesis to calc the precision for
-* \param n count of grams
-* \return precision p_n
-*/
-double precision(Hypothesis* C, unsigned int n)
-{
-	// TODO
-	return 0;
-}
-
-/**
-*
-*
-* \return Brevity Penalty
-*/
-double brevity_penalty()
-{
-	// TODO
-	return 0;
-}
-
-/**
-* calcs the BLEU-score for a Hypothesis
-*
-* \param N
-* \param H reference to a Hypothesis to calc the BLEU-score for
-* \return BLEU_score
-*/
-double BLEU_score(unsigned int N, Hypothesis* H)
-{
-	if (N==0)
-	{
-		cerr << "BLEU_score: invalid N" << endl;
-		return 0;
-	}
-	/* um die Summe zu berechnen:
-	* 1. 1/N kann man ausklammern
-	* 2. log(a)+log(b) = log(a*b) => es reicht die p_n zu multiplizieren und am Ende einmal den log zu berechnen.
-	*/
-	double akku=0;
-	for (unsigned int i=1; i<=N; i++)
-		akku *= precision(H, i);
-
-	return brevity_penalty()*exp(log(akku)/(double)N);
-}
-
 int main(int argc, char* argv[])
 {
 
