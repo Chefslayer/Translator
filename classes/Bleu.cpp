f85eff448f7bd4e8b494c19ddf8128b24f0e8f7f
Robert Heim <robhei@riggs.informatik.RWTH-Aachen.DE>
2009-05-23 09:50:44 +0200

    Einrückungen, Optimierung, Bugs

--- classes/Bleu.cpp
+++ classes/Bleu.cpp
@@ -16,39 +16,35 @@ Bleu::Bleu(vector<unsigned int> translation, vector<unsigned int> reference)
 int Bleu::nGramsMatching(unsigned int n)
 {
 	int num = 0;
-	unsigned int temp = 0;
-	for (int i = 0; i < ref.size();i++)
+	for (int i = 0; i + n < ref.size(); i++)
 	{
-		for (int transposition = 0; transposition < trans.size(); transposition++)
+		for (int transposition = 0; transposition + n < trans.size(); transposition++)
 		{
 			if (trans[transposition] == ref[i])
 			{
-				temp = 1;
-				for (int j = 1; j < n; j++)
+				int j=1;
+				while (		j < n
+						&& (i+j) < ref.size()
+						&& (transposition + j) < trans.size()
+						&& trans[transposition + j] == ref[i + j]
+				      								) j++;
+				if (j == n)
 				{
-					if ((i+j) >= ref.size() or (transposition + j) >= trans.size())
-					{
-						break;
-					}
-					else
-						if (trans[transposition + j] == ref[i + j]) temp++;
+					num++;
+					//break; nicht breaken, weil auch ab einer position im ref-string mehrmals im trans-string gematched werden kann, oder irre ich mich?
 				}
-			if (temp == n)
-			{
-				num++;
-				break;
-			}
 			}
 		}
 	}
 	return num;
 }
+
 double Bleu::precision(unsigned int n)
 {
 	double precision = 0;
 	if ((ref.size() + 1 - n) <= 0)
 	{
-		cout << "Hilfe durch 0 teilen!!!!" << endl;
+		cerr << "Bleu::precision: Division durch 0." << endl;
 		return -1;
 	}
 	precision = (double)nGramsMatching(n)/(double)(ref.size() + 1 - n);
@@ -62,22 +58,18 @@ double Bleu::brevityPenalty()
 
 	if (c > r)
 		return 1;
-	if (c <= r)
-	{
-		//cout << "r:" << (double)r << "c:" << (double)c << "(1-r)/c:" << (double)(1-r)/(double)c <<  endl;
-		if (c <= 0)
-			cout << "Die Länge der Referenzübersetzung ist <= 0..." << endl;
+	if (c > 0) // (c <= r) gilt hier eh
 		return (double)exp(((double)(1-r)/(double)c));
-	}
-//	else
-//		return 0;
+
+	cerr << "Bleu::brevityPenalty(): Fehler bei der Berechnung. (Länge der Referenzübersetzung <= 0)" << endl;
+	return -1;
 }
 
 double Bleu::bleuScore(unsigned int N)
 {
 	if (N==0)
 	{
-		cerr << "BLEU_score: invalid N" << endl;
+		cerr << "Bleu::bleuScore: ungültiges N (0)" << endl;
 		return 0;
 	}
 	/* um die Summe zu berechnen:
@@ -87,9 +79,9 @@ double Bleu::bleuScore(unsigned int N)
 	double akku = 1.0;
 	for (unsigned int i=1; i<=N; i++)
 	{
-	//	akku = akku + log((double)precision(i));
-	akku = akku * precision(i);
+	//	akku += log((double)precision(i));
+		akku *= precision(i);
 	//	cout << "precision:" << precision(i) << endl;
 	}
-		return brevityPenalty()*exp(log(akku)/(double)N);
+	return brevityPenalty()*exp(log(akku)/(double)N);
 }
