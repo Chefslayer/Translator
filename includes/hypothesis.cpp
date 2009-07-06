158a35802d5695f35d800184d670b011adad3d84
Robert Heim <robhei@lea.informatik.rwth-aachen.de>
2009-07-06 16:37:46 +0200

    hypothesen korrigiert

--- includes/hypothesis.cpp
+++ includes/hypothesis.cpp
@@ -19,6 +19,22 @@ Hypothesis::Hypothesis(Hypothesis *p ,double fCost, double eCost, unsigned int t
 	trans = t;
 }
 
+Hypothesis::Hypothesis(Hypothesis *p ,double fCost, double eCost, vector<unsigned int> &t)
+{
+	prevHyp = p;
+	if (p!=NULL)
+	{
+		costs.push_back(p->costs[0]+fCost);
+		costs.push_back(p->costs[1]+eCost);
+	}
+	else
+	{
+		costs.push_back(fCost);
+		costs.push_back(eCost);
+	}
+	phraseTrans = t;
+}
+
 bool cmp_Hyp(Hypothesis* i, Hypothesis* j)
 {
 	return (i->costs[0] < j->costs[0]);
