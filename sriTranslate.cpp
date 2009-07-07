70a3647e4edc6499481fdfa0aac023e053779dc1
Chefslayer <inga.herber@web.de>
2009-07-07 23:58:33 +0200

    Lexicon in vocab geändert,…segfault…

--- sriTranslate.cpp
+++ sriTranslate.cpp
@@ -28,8 +28,7 @@
 
 using namespace std;
 
-static Vocab f;
-static Vocab e;
+static Vocab vocab;
 
 /*
  * holds information about a phrasepair from a translation-table.
@@ -47,7 +46,135 @@ struct trans_phrase_tab_struct
 	vector<unsigned int> e;
 };
 
+/**
+ * holds information about a translation of a phrase
+ */
+struct phrase_translation_struct
+{
+	/// relative Frequence of F
+	double relFreqF;
+	/// relative Frequence of E
+	double relFreqE;
+	/// the length of the phrase in f
+	unsigned int phraseLength;
+	/// word-codes of the target-phrase
+	vector<unsigned int> e;
+};
 
+/** Prunes the Stack to KEEP_N_BEST_HYPOS elements.
+ *
+ *   \param s reference to stack of hypthesis
+ *   \return reference to best Hypothesis in s
+ */
+Hypothesis* pruneStack(stack < Hypothesis* > &s)
+{
+	vector<Hypothesis*> v;
+	unsigned int size = s.size();
+	v.resize(size);
+
+	for (unsigned int i=0; !s.empty(); i++)
+	{
+		v[i] = s.top();
+		s.pop();
+	}
+	sort(v.begin(), v.end(), cmp_Hyp);
+
+	for (unsigned int i=0; i<size && i<KEEP_N_BEST_HYPOS ;i++)
+	{
+		s.push(v[i]);
+	}
+	//return best Hypo
+	return v[0];
+}
+
+/**
+ * searches a translation for a given line of text.
+ *
+ * \param words a vector of words which will be translatet
+ * \param translationtab table of translations
+ * \return best Hypothesis
+ */
+Hypothesis* searchTranslation(vector<unsigned int> &words, vector<trans_phrase_tab_struct>  &translationtab)
+{
+	Hypothesis* minCostsHyp;
+	vector< stack < Hypothesis* > > stacks;
+	stacks.resize((words.size()+1));
+	vector<unsigned int> tmp;
+	tmp.push_back(0);
+	Hypothesis* h = new Hypothesis(NULL,0,0,tmp);
+	stacks[0].push(h);
+
+	for (unsigned int stackNr = 0; stackNr < stacks.size()-1; stackNr++)
+	{
+//		cout << stackNr << endl;
+		vector<phrase_translation_struct> phrases;
+		phrases.clear();
+
+		bool found_at_least_one_hypo = false;
+
+		for (unsigned int phraseLength = 1; phraseLength <= MAX_PHRASE_LENGTH && stackNr+phraseLength<words.size(); phraseLength++)
+		{
+			vector<unsigned int> phraseF;
+			phraseF.clear();
+			for (unsigned int k = 0; k < phraseLength; k++)
+			{
+				phraseF.push_back(words[stackNr+k]);
+			}
+			// search for all possible translations and save it for later to create the hypos
+
+			// search for first occurance of phraseF in transtab
+			unsigned int transTabPos = 0;
+			while (transTabPos < translationtab.size() && phraseF != translationtab[transTabPos].f)
+			{
+				transTabPos++;
+			}
+			// search for all following occurances of phraseF in transtab
+			while (transTabPos < translationtab.size() && phraseF == translationtab[transTabPos].f)
+			{
+				found_at_least_one_hypo = true;
+				phrase_translation_struct currentTranslation;
+
+				currentTranslation.relFreqF = translationtab[transTabPos].relFreqF; 
+				currentTranslation.relFreqE = translationtab[transTabPos].relFreqE;
+				currentTranslation.phraseLength = phraseLength;
+				currentTranslation.e = translationtab[transTabPos].e;
+
+				phrases.push_back(currentTranslation);
+				transTabPos++;
+			}
+		}
+		if (!found_at_least_one_hypo)
+		{
+			// insert '?'
+			phrase_translation_struct currentTranslation;
+
+			currentTranslation.relFreqF = 20;
+			currentTranslation.relFreqE = 20;
+			currentTranslation.phraseLength = 1;
+			vector<unsigned int> tmp(0);
+			currentTranslation.e = tmp;
+
+			phrases.push_back(currentTranslation);
+		}
+
+		// create for each hypo in the current stack the new hypos of the current phrases
+		while (!stacks[stackNr].empty())
+		{
+			Hypothesis *prev = stacks[stackNr].top();
+
+			for (vector<phrase_translation_struct>::iterator it = phrases.begin(); it != phrases.end(); it++)
+			{
+				Hypothesis *h = new Hypothesis(prev, it->relFreqF, it->relFreqE, it->e);
+				stacks[ stackNr + it->phraseLength ].push(h);
+			}
+			stacks[stackNr].pop();
+		}
+		minCostsHyp = pruneStack(stacks[stackNr+1]);
+	}
+
+	// return best Hypothesis of the last stack
+	return minCostsHyp;
+}
 
 int main(int argc, char* argv[])
 {
@@ -100,20 +227,25 @@ int main(int argc, char* argv[])
 		current.relFreqE = temp;
 
 		vector<string> tempV;
-
+		VocabIndex *vocabBuffer(new VocabIndex[50]);
 		// insert src phras
 		tempV = stringSplit(line_vec[1], " ");
-		for (int j = 0; j <= line_vec[1].size(); j++)
+		// wofür wird das gebraucht und wie ist die syntax?
+		VocabIndex seIndex();
+		for (int j = line_vec[1].size(); j > 0; j--)
 			{
-				current.f.push_back(f.addWord(tempV[j].c_str()));
+				current.f.push_back(vocab.addWord(tempV[j].c_str()));
 			}
+		// wofür wird das gebraucht und wie ist die syntax?
+		VocabIndex ssIndex();
+
 
 		// insert target phrase
 		tempV.clear();
 		tempV = stringSplit(line_vec[2], " ");
-		for (int k = 0; k <= line_vec[2].size(); k++)
+		for (int k = line_vec[2].size(); k > 0; k--)
 			{
-				current.e.push_back(e.addWord(tempV[k].c_str()));
+				current.e.push_back(vocab.addWord(tempV[k].c_str()));
 			}
 
 		if (i >= trans_phrase_tab_vec.size())
@@ -131,16 +263,16 @@ int main(int argc, char* argv[])
 
 	// translate src_doc
 	int c = 0;
-	/*while (getline(src_doc, line))
+	while (getline(src_doc, line))
 	{
 		c++;
 		if (c > 10) break;
 		vector<string> stringwords = stringSplit(line, " ");
-		vector<unsigned int> words(stringwords.size(), 0);
+		vector<VocabIndex> words(stringwords.size(), 0);
 
 		for (unsigned int i = 0; i < stringwords.size(); i++)
 		{
-			words[i] = f.getNum(stringwords[i]);
+			words[i] = vocab.addWord(stringwords[i].c_str());
 		}
 
 		Hypothesis* transHyp = searchTranslation(words, trans_phrase_tab_vec);
@@ -150,7 +282,7 @@ int main(int argc, char* argv[])
 		{
 			string tmp="";
 			for (unsigned int i = 0; i < transHyp->phraseTrans.size(); ++i ){
-				tmp += (tmp== ""?"":" ") + e.getWord(transHyp->phraseTrans[i]);
+			//	tmp += (tmp== ""?"":" ") + vocab.addWord((transHyp->phraseTrans[i]).c_str());
 			}
 			translation = tmp+" "+translation;
 
@@ -158,7 +290,6 @@ int main(int argc, char* argv[])
 		}
 		cout << translation << endl;
 	}
-	*/
 
 	return 0; //EXIT_SUCCESS;
 }
